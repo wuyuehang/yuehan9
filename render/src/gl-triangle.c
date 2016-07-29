74173db394ed578d753505d3b00aeab3f63ef087
Yuehang Wu <yuehan9.wu@gmail.com>
2016-07-29 13:47:18 +0800

    refine opengl triangle strip and fans style
    Signed-off-by: Yuehang Wu <yuehan9.wu@gmail.com>

--- render/src/gl-triangle.c
+++ render/src/gl-triangle.c
@@ -1,20 +1,61 @@
 #include <GL/glut.h>
 
+static unsigned int style = 0;
+
 void displayCB(void)		/* function called whenever redisplay needed */
 {
 	glClear(GL_COLOR_BUFFER_BIT);		/* clear the display */
-	glColor3f(1.0, 1.0, 1.0);		/* set current color to white */
-	glBegin(GL_POLYGON);			/* draw filled triangle */
-	glVertex2i(300,200);			/* specify each vertex of triangle */
-	glVertex2i(400,400);
-	glVertex2i(200,400);
-	glEnd();				/* OpenGL draws the filled triangle */
+
+	switch (style) {
+	case 0:
+		glColor3f(1.0, 1.0, 1.0);		/* set current color to white */
+		glBegin(GL_TRIANGLES);			/* draw filled triangle */
+		glVertex2i(300,200);			/* specify each vertex of triangle */
+		glVertex2i(400,400);
+		glVertex2i(200,400);
+		glEnd();				/* OpenGL draws the filled triangle */
+		break;
+	case 1:
+		glBegin(GL_TRIANGLE_STRIP);
+		glColor3f(1.0, 1.0, 1.0);
+		glVertex2i(100, 100);
+		glColor3f(1.0, 0.0, 1.0);
+		glVertex2i(180, 500);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex2i(290, 480);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex2i(390, 370);
+		glColor3f(0.0, 0.0, 1.0);
+		glVertex2i(480, 130);
+		glEnd();
+		break;
+	case 2:
+		glBegin(GL_TRIANGLE_FAN);
+		glColor3f(1.0, 1.0, 1.0);
+		glVertex2i(100, 100); /* root */
+
+		glColor3f(1.0, 0.0, 1.0);
+		glVertex2i(180, 500);
+		glColor3f(1.0, 0.0, 0.0);
+		glVertex2i(290, 480);
+		glColor3f(0.0, 1.0, 0.0);
+		glVertex2i(390, 370);
+		glColor3f(0.0, 0.0, 1.0);
+		glVertex2i(480, 130);
+		glEnd();
+		break;
+	}
+
 	glFlush();				/* Complete any pending operations */
 }
 
 void keyCB(unsigned char key, int x, int y)
 {
-	if(key == 'q') exit(0);
+	if (key == 'q') exit(0);
+	else if (key == ' ') {
+		style = (style > 2) ? 0 : (style + 1);
+		glutPostRedisplay();
+	}
 }
 
 
