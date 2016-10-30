7340649153af5aed7282193ca884c74d42bd85c4
Yuehang Wu <yuehan9.wu@gmail.com>
2016-10-30 17:43:26 +0800

    add update uniform of glsl example
    Signed-off-by: Yuehang Wu <yuehan9.wu@gmail.com>

--- render/src/minimal.c
+++ render/src/minimal.c
@@ -3,14 +3,23 @@
 #include <GL/glut.h>
 #include <GL/gl.h>
 #include <GL/glext.h>
+#include <math.h>
+#include <assert.h>
 
+/* handle of internal opengl object */
 GLuint g_vbo;
 GLuint g_program;
+GLuint g_scalex;
+GLuint g_scaley;
+GLuint g_world_mat;
 
 static const char *vs_src[] = {"#version 430\n"
   "layout (location = 0) in vec3 Position;"
+  "uniform float scalex;"
+  "uniform float scaley;"
+  "uniform mat4 world;"
   "void main(){"
-  "gl_Position = vec4(Position.x, Position.y, Position.z, 1.0);}"};
+  "gl_Position = world * vec4(scalex * Position.x, scaley * Position.y, Position.z, 1.0);}"};
 
 static const char *ps_src[] = {"#version 430\n"
   "out vec4 color;"
@@ -20,12 +29,15 @@ static const char *ps_src[] = {"#version 430\n"
 void compile_shader()
 {
   g_program = glCreateProgram();
+  assert(g_program != 0);
 
   GLuint vs_shader = glCreateShader(GL_VERTEX_SHADER);
+  assert(vs_shader != 0);
   glShaderSource(vs_shader, 1, vs_src, NULL);
   glCompileShader(vs_shader);
 
   GLuint ps_shader = glCreateShader(GL_FRAGMENT_SHADER);
+  assert(ps_shader != 0);
   glShaderSource(ps_shader, 1, ps_src, NULL);
   glCompileShader(ps_shader);
 
@@ -35,6 +47,13 @@ void compile_shader()
   glLinkProgram(g_program);
 
   glUseProgram(g_program);
+
+  g_scalex = glGetUniformLocation(g_program, "scalex");
+  assert(g_scalex != 0xFFFFFFFF);
+  g_scaley = glGetUniformLocation(g_program, "scaley");
+  assert(g_scaley != 0xFFFFFFFF);
+  g_world_mat = glGetUniformLocation(g_program, "world");
+  assert(g_world_mat != 0xFFFFFFFF);
 }
 
 void keycb(unsigned char key, int x, int y)
@@ -44,6 +63,20 @@ void keycb(unsigned char key, int x, int y)
 
 void render()
 {
+  static float scale = 0.0;
+
+  scale += 0.001;
+
+  glUniform1f(g_scalex, sinf(scale));
+  glUniform1f(g_scaley, cosf(scale));
+
+  GLfloat world[4][4] = {{1.0f, 0.0f, 0.0f, sinf(scale)},
+    {0.0f, 1.0f, 0.0f, cosf(scale)},
+    {0.0f, 0.0f, 1.0f, 0.0f},
+    {0.0f, 0.0f, 0.0f, 1.0f}};
+
+  glUniformMatrix4fv(g_world_mat, 1, GL_TRUE, &world[0][0]);
+
   glClear(GL_COLOR_BUFFER_BIT);
 
   glEnableVertexAttribArray(0);
@@ -64,6 +97,7 @@ void resource()
   GLfloat position_vb[3][3] = {{-0.5f, -0.5f, 0.0f}, {0.5f, -0.5f, 0.0f}, {0.0f, 0.5f, 0.0f}};
 
   glGenBuffers(1, &g_vbo);
+  assert(g_vbo != 0xFFFFFFFF);
 
   glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
 
@@ -82,7 +116,7 @@ int main(int argc, char**argv)
 
   glutKeyboardFunc(keycb);
 
-  glutDisplayFunc(render);
+  glutIdleFunc(render);
 
   glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
 
