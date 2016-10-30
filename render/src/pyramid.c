37c148410b9d44a17f84d1891f0a2bb4b919dc57
Yuehang Wu <yuehan9.wu@gmail.com>
2016-10-30 21:41:33 +0800

    add index draw example
    Signed-off-by: Yuehang Wu <yuehan9.wu@gmail.com>

--- render/src/pyramid.c
+++ render/src/pyramid.c
@@ -8,19 +8,16 @@
 
 /* handle of internal opengl object */
 GLuint g_vbo;
+GLuint g_ibo;
 GLuint g_program;
-GLuint g_scalex;
-GLuint g_scaley;
 GLuint g_world_mat;
 
 static const char *vs_src[] = {"#version 430\n"
   "layout (location = 0) in vec3 Position;"
-  "uniform float scalex;"
-  "uniform float scaley;"
   "uniform mat4 world;"
   "out vec4 color;"
   "void main(){"
-  "gl_Position = world * vec4(scalex * Position.x, scaley * Position.y, Position.z, 1.0);"
+  "gl_Position = world * vec4(Position.x, Position.y, Position.z, 1.0);"
   "color = vec4(clamp(Position, 0.0, 1.0), 1.0);}"};
 
 static const char *ps_src[] = {"#version 430\n"
@@ -51,10 +48,6 @@ void compile_shader()
 
   glUseProgram(g_program);
 
-  g_scalex = glGetUniformLocation(g_program, "scalex");
-  assert(g_scalex != 0xFFFFFFFF);
-  g_scaley = glGetUniformLocation(g_program, "scaley");
-  assert(g_scaley != 0xFFFFFFFF);
   g_world_mat = glGetUniformLocation(g_program, "world");
   assert(g_world_mat != 0xFFFFFFFF);
 }
@@ -70,12 +63,10 @@ void render()
 
   scale += 0.002;
 
-  glUniform1f(g_scalex, sinf(scale));
-  glUniform1f(g_scaley, cosf(scale));
-
-  GLfloat world[4][4] = {{1.0f, 0.0f, 0.0f, sinf(scale)},
-    {0.0f, 1.0f, 0.0f, cosf(scale)},
-    {0.0f, 0.0f, 1.0f, 0.0f},
+  /* rotate with y axis */
+  GLfloat world[4][4] = {{cosf(scale), 0.0f, -sinf(scale), 0.0f},
+    {0.0f, 1.0f, 0.0f, 0.0f},
+    {sinf(scale), 0.0f, cosf(scale), 0.0f},
     {0.0f, 0.0f, 0.0f, 1.0f}};
 
   glUniformMatrix4fv(g_world_mat, 1, GL_TRUE, &world[0][0]);
@@ -86,9 +77,11 @@ void render()
 
   glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
 
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ibo);
+
   glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, 0);
 
-  glDrawArrays(GL_TRIANGLES, 0, 3);
+  glDrawElements(GL_TRIANGLES, 12, GL_UNSIGNED_INT, 0);
 
   glDisableVertexAttribArray(0);
 
@@ -97,14 +90,22 @@ void render()
 
 void resource()
 {
-  GLfloat position_vb[3][3] = {{-0.5f, -0.5f, 0.0f}, {0.5f, -0.5f, 0.0f}, {0.0f, 0.5f, 0.0f}};
+  GLfloat position_vb[4][3] = {{-0.5f, -0.5f, 0.0f},
+    {0.0f, -0.5f, 0.5f},
+    {0.5f, -0.5f, 0.0f},
+    {0.0f, 0.5f, 0.0f}};
 
   glGenBuffers(1, &g_vbo);
   assert(g_vbo != 0xFFFFFFFF);
-
   glBindBuffer(GL_ARRAY_BUFFER, g_vbo);
-
   glBufferData(GL_ARRAY_BUFFER, sizeof(position_vb), position_vb, GL_STATIC_DRAW);
+
+  GLuint index_vb[] = {0, 3, 1, 1, 3, 2, 2, 3, 0, 0, 1, 2};
+
+  glGenBuffers(1, &g_ibo);
+  assert(g_ibo != 0xFFFFFFFF);
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, g_ibo);
+  glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(index_vb), index_vb, GL_STATIC_DRAW);
 }
 
 int main(int argc, char**argv)
