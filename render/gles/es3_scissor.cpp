24e65be400e674c8a3542502ecdea4205ca81744
Yuehang Wu <yuehan9.wu@gmail.com>
2017-01-17 18:04:26 +0800

    add scissor test for es and gl
    Signed-off-by: Yuehang Wu <yuehan9.wu@gmail.com>

---
+++ render/gles/es3_scissor.cpp
@@ -0,0 +1,150 @@
+#include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <glm/glm.hpp>
+#include "utility.h"
+#include "warp_types.h"
+
+#define QUALD 64
+#define QUALD_SQRT 8
+
+struct warp_runtime *wrt = NULL;
+
+int main(int argc, char **argv)
+{
+	wrt = (struct warp_runtime *)malloc(sizeof(struct warp_runtime));
+	assert(NULL != wrt);
+
+	memset(wrt, 0, sizeof(struct warp_runtime));
+
+	create_warp_runtime(wrt);
+
+	glViewport(0, 0, 500, 500);
+
+	glEnable(GL_SCISSOR_TEST);
+	glScissor(0, 0, 250, 250);
+
+	/* setup shader & program */
+	GLchar message[512];
+	GLint status;
+	GLchar *source = NULL;
+
+	GLuint vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	load_shader_from_file("shaders/es3_instance_1.vert", &source);
+	glShaderSource(vertex_shader, 1, (const GLchar * const*)&source, NULL);
+	glCompileShader(vertex_shader);
+
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &status);
+	if (!status) {
+		glGetShaderInfoLog(vertex_shader, 512, NULL, message);
+		printf("%s\n", message);
+	}
+
+	GLuint fragment_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	free(source);
+	load_shader_from_file("shaders/es3_instance_1.frag", &source);
+	glShaderSource(fragment_shader, 1, (const GLchar * const*)&source, NULL);
+	glCompileShader(fragment_shader);
+
+	glGetShaderiv(fragment_shader, GL_COMPILE_STATUS, &status);
+	if (!status) {
+		glGetShaderInfoLog(fragment_shader, 512, NULL, message);
+		printf("%s\n", message);
+	}
+
+	GLuint prog = glCreateProgram();
+	glAttachShader(prog, vertex_shader);
+	glAttachShader(prog, fragment_shader);
+	glLinkProgram(prog);
+
+	glGetProgramiv(prog, GL_LINK_STATUS, &status);
+	if (!status) {
+		glGetProgramInfoLog(prog, 512, NULL, message);
+		printf("%s\n", message);
+	}
+
+	glUseProgram(prog);
+
+	/* set up resource */
+	GLfloat pos_buf[] = {
+			-0.02f, 0.02f,
+			0.02f, 0.02f,
+			0.02f, -0.02f,
+			-0.02f, 0.02f,
+			0.02f, -0.02f,
+			-0.02f, -0.02f,
+	};
+
+	GLuint vao;
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+
+	/* setup array buffer for position and color
+	 */
+	GLuint vbo_pos;
+	glGenBuffers(1, &vbo_pos);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_pos);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pos_buf), pos_buf, GL_STATIC_DRAW);
+
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid *)0);
+
+	GLuint vbo_inst;
+	glGenBuffers(1, &vbo_inst);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_inst);
+
+	glm::vec2 go[QUALD];
+	int index = 0;
+	GLfloat rate = -0.90;
+
+	for (int i = 0; i < QUALD_SQRT; i++) {
+		for (int j = 0; j < QUALD_SQRT; j++) {
+			glm::vec2 move;
+			move.x = (GLfloat)i * (2.0f / QUALD_SQRT) + rate;
+			move.y = (GLfloat)j * (2.0f / QUALD_SQRT) + rate;
+			go[index++] = move;
+		}
+	}
+
+	glBufferData(GL_ARRAY_BUFFER, sizeof(glm::vec2) * QUALD, &go[0], GL_STATIC_DRAW);
+	glEnableVertexAttribArray(1);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid *)0);
+	glVertexAttribDivisor(1, 1);
+
+	/* game loop */
+	while (1) {
+
+		XNextEvent(wrt->x11_display, &(wrt->event));
+
+		switch(wrt->event.type) {
+		case Expose:
+			/* do the render */
+			glClearColor(0.3f, 0.3f, 0.3f, 1.0f);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			glBindVertexArray(vao);
+			glDrawArraysInstanced(GL_TRIANGLES, 0, 6, QUALD);
+			glBindVertexArray(0);
+
+			eglSwapBuffers(wrt->egl_display, wrt->egl_surface);
+			break;
+		case ButtonPress:
+		case KeyPress:
+			goto finish;
+		default:
+			break;
+		}
+	}
+
+finish:
+	glDeleteBuffers(1, &vbo_pos);
+	glDeleteBuffers(1, &vbo_inst);
+	glDeleteVertexArrays(1, &vao);
+	glDeleteShader(vertex_shader);
+	glDeleteShader(fragment_shader);
+	glDeleteProgram(prog);
+
+	destroy_warp_runtime(wrt);
+	return 0;
+}
