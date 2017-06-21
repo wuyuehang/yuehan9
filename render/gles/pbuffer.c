0aadbbda4af44dfd475f584c19f2b278c251a0fc
Yuehang Wu <yuehan9.wu@gmail.com>
2017-06-21 15:45:29 +0800

    add pbuffer offscreen render demo
    Signed-off-by: Yuehang Wu <yuehan9.wu@gmail.com>

---
+++ render/gles/pbuffer.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <SOIL/SOIL.h>
+#include "warp_types.h"
+
+int main()
+{
+	struct offscreen_warp *ow;
+	ow = (struct offscreen_warp *)malloc(sizeof(*ow));
+	memset(ow, 0, sizeof(*ow));
+	unsigned char *blob = (unsigned char *)malloc(OFFSCREEN_SURFACE_WIDTH * OFFSCREEN_SURFACE_HEIGHT * 4);
+
+	create_offscreen_warp(ow);
+
+	// preamble
+	create_offscreen_warp_shaders(ow,
+			&ow->vertex_shader[0], "shaders/es3_draw_tri.vert",
+			&ow->fragment_shader[0], "shaders/es3_draw_tri.frag");
+
+	create_offscreen_warp_program(&ow->vertex_shader[0],
+			&ow->fragment_shader[0],
+			&ow->program[0]);
+
+	// rendering happen
+	GLfloat pos_buf[] = {
+		-0.5, -0.5,
+		0.0, 0.5,
+		0.5, -0.5
+	};
+
+	GLushort idx_buf[] = {
+		0, 1, 2
+	};
+
+	GLuint vao, vbo_idx, vbo_pos;
+	glGenVertexArrays(1, &vao);
+	glGenBuffers(1, &vbo_idx);
+	glGenBuffers(1, &vbo_pos);
+
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, vbo_idx);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(idx_buf), idx_buf, GL_STATIC_DRAW);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo_pos);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pos_buf), pos_buf, GL_STATIC_DRAW);
+
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(GLfloat), (GLvoid *)0);
+	glEnableVertexAttribArray(0);
+
+	glDrawElements(GL_TRIANGLES, 3, GL_UNSIGNED_SHORT, (void *)0);
+
+	// dump the image
+	glReadPixels(0, 0, OFFSCREEN_SURFACE_WIDTH, OFFSCREEN_SURFACE_HEIGHT, GL_RGBA, GL_UNSIGNED_BYTE, blob);
+	SOIL_save_image(OFFSCREEN_IMAGE,
+			SOIL_SAVE_TYPE_BMP,
+			OFFSCREEN_SURFACE_WIDTH,
+			OFFSCREEN_SURFACE_HEIGHT,
+			4, blob);
+	destroy_offscreen_warp(ow);
+
+	return 0;
+}
--- render/gles/warp_types.h
+++ render/gles/warp_types.h
@@ -1,7 +1,12 @@
+#ifndef _WARP_TYPES_H
+#define _WARP_TYPES_H
+
 #include <GLES3/gl3.h>
 #include <EGL/egl.h>
 #include <EGL/eglext.h>
 #include <X11/Xlib.h>
+#include <assert.h>
+#include "utility.h"
 
 struct warp_runtime {
 	/* X11 stuff */
@@ -89,3 +94,145 @@ void destroy_warp_runtime(struct warp_runtime *wrt)
 	XCloseDisplay(wrt->x11_display);
 	free(wrt);
 }
+
+#define OFFSCREEN_IMAGE	__FILE__".bmp"
+#define OFFSCREEN_SURFACE_WIDTH	16
+#define OFFSCREEN_SURFACE_HEIGHT	16
+// offscreen EGL config
+EGLint offscrn_conf_attrib[] =
+		{
+			EGL_RED_SIZE, 8,
+			EGL_GREEN_SIZE, 8,
+			EGL_BLUE_SIZE,  8,
+			EGL_ALPHA_SIZE, 8,
+			EGL_DEPTH_SIZE, 24,
+			EGL_STENCIL_SIZE, EGL_DONT_CARE,
+			EGL_SAMPLE_BUFFERS, 0,
+			EGL_RENDERABLE_TYPE, EGL_OPENGL_ES3_BIT_KHR,
+			EGL_SURFACE_TYPE, EGL_PBUFFER_BIT,
+			EGL_NONE
+		};
+
+// offscreen EGL context
+EGLint offscrn_ctx_attrib[] =
+		{
+			EGL_CONTEXT_CLIENT_VERSION, 3,
+			EGL_NONE
+		};
+
+// offscreen EGL surface
+EGLint offscrn_surface_attrib[] =
+		{
+			EGL_WIDTH, OFFSCREEN_SURFACE_WIDTH,
+			EGL_HEIGHT, OFFSCREEN_SURFACE_WIDTH,
+			EGL_NONE
+		};
+
+struct offscreen_warp {
+	// offscreen EGL blob
+	EGLDisplay disp;
+	EGLContext ctx;
+	EGLSurface sfc;
+	EGLContext cfg;
+	EGLint major;
+	EGLint minor;
+	// offscreen gles blob
+	GLuint vertex_shader[2];
+	GLuint fragment_shader[2];
+	GLuint program[2];
+};
+
+void create_offscreen_warp(struct offscreen_warp *ow)
+{
+	ow->disp = eglGetDisplay(EGL_DEFAULT_DISPLAY);
+	assert(ow->disp != EGL_NO_DISPLAY);
+
+	assert(EGL_TRUE == eglInitialize(ow->disp, &ow->major, &ow->minor));
+
+	EGLint numc = 0;
+	assert(EGL_TRUE == eglChooseConfig(ow->disp, offscrn_conf_attrib, &ow->cfg, 1, &numc));
+
+	ow->ctx = eglCreateContext(ow->disp, ow->cfg, EGL_NO_CONTEXT, offscrn_ctx_attrib);
+	assert(ow->ctx != EGL_NO_CONTEXT);
+
+	ow->sfc = eglCreatePbufferSurface(ow->disp, ow->cfg, offscrn_surface_attrib);
+	assert(ow->sfc != EGL_NO_SURFACE);
+
+	eglMakeCurrent(ow->disp, ow->sfc, ow->sfc, ow->ctx);
+
+	// preamble operations
+	glViewport(0, 0, OFFSCREEN_SURFACE_WIDTH, OFFSCREEN_SURFACE_HEIGHT);
+	glEnable(GL_DEPTH_TEST);
+	glDepthFunc(GL_LESS);
+
+	glClearColor(0.0, 0.0, 0.0, 1.0);
+	glClearDepthf(1.0);
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+}
+
+void destroy_offscreen_warp(struct offscreen_warp *ow)
+{
+	eglMakeCurrent(ow->disp, EGL_NO_SURFACE, EGL_NO_SURFACE, EGL_NO_CONTEXT);
+	eglDestroyContext(ow->disp, ow->ctx);
+	eglDestroySurface(ow->disp, ow->sfc);
+	eglTerminate(ow->disp);
+	free(ow);
+}
+
+void create_offscreen_warp_shaders(struct offscreen_warp *ow, GLuint *vsidx, char *vspath, GLuint *fsidx, char *fspath)
+{
+	GLchar message[512];
+	GLint status;
+	GLchar *source = NULL;
+
+	if (vspath != NULL) {
+		*vsidx = glCreateShader(GL_VERTEX_SHADER);
+		load_shader_from_file(vspath, &source);
+		glShaderSource(*vsidx, 1, (const GLchar * const*)&source, NULL);
+		glCompileShader(*vsidx);
+
+		glGetShaderiv(*vsidx, GL_COMPILE_STATUS, &status);
+		if (!status) {
+			glGetShaderInfoLog(*vsidx, 512, NULL, message);
+			printf("%s\n", message);
+		}
+		free(source);
+	}
+
+	if (fspath != NULL) {
+		*fsidx = glCreateShader(GL_FRAGMENT_SHADER);
+		load_shader_from_file(fspath, &source);
+		glShaderSource(*fsidx, 1, (const GLchar * const*)&source, NULL);
+		glCompileShader(*fsidx);
+
+		glGetShaderiv(*fsidx, GL_COMPILE_STATUS, &status);
+		if (!status) {
+			glGetShaderInfoLog(*fsidx, 512, NULL, message);
+			printf("%s\n", message);
+		}
+		free(source);
+	}
+}
+
+void create_offscreen_warp_program(GLuint *vsidx, GLuint *fsidx, GLuint *prog)
+{
+	GLint status;
+	GLchar message[512];
+	assert(*fsidx);
+	assert(*vsidx);
+
+	*prog = glCreateProgram();
+	glAttachShader(*prog, *vsidx);
+	glAttachShader(*prog, *fsidx);
+	glLinkProgram(*prog);
+
+	glGetProgramiv(*prog, GL_LINK_STATUS, &status);
+	if (!status) {
+		glGetProgramInfoLog(*prog, 512, NULL, message);
+		printf("%s\n", message);
+	}
+
+	glUseProgram(*prog);
+}
+
+#endif
