75ee513a023b91347e0d051edd158be20abaa61d
Yuehang Wu <yuehan9.wu@gmail.com>
2017-09-16 16:01:52 +0800

    enable log shader and program properties
    Signed-off-by: Yuehang Wu <yuehan9.wu@gmail.com>

--- render/src/glrunner.h
+++ render/src/glrunner.h
@@ -47,11 +47,15 @@ public:
 	~GlRunner();
 	static void OnKeyboard(GLFWwindow*, int, int, int, int);
 	GLuint BuildShaderProgram(const char *filename, GLenum type);
+	void LogProgramInputAttribs(GLuint prog);
+	void LogProgramUniform(GLuint prog);
+	void LogProgramUniformBlock(GLuint prog);
 	GLuint BuildProgramPipeline();
 	GLuint BuildXfb(const char *filename, GLsizei count, const char **varyings, GLenum mode);
 	void UpdateKeyboardCB(GLFWkeyfun);
 	void OnRender();
 	void DrawQuad(GLuint);
+	const char* ConvertTypeToString(GLenum type);
 	GLFWwindow* win() {
 		return this->_win;
 	}
@@ -242,7 +246,142 @@ GLuint GlRunner::BuildShaderProgram(const char *filename, GLenum type)
 
 	ShaderObj = glCreateShaderProgramv(type, 1, &token);
 
+	std::cout << std::endl << filename << std::endl;
+
+	LogProgramInputAttribs(ShaderObj);
+
+	LogProgramUniform(ShaderObj);
+
+	LogProgramUniformBlock(ShaderObj);
+
 	return ShaderObj;
+}
+
+void GlRunner::LogProgramInputAttribs(GLuint prog)
+{
+	GLint numAttirb;
+	GLenum properties[3] = {
+		GL_NAME_LENGTH,
+		GL_TYPE,
+		GL_LOCATION
+	};
+
+	glGetProgramInterfaceiv(prog, GL_PROGRAM_INPUT, GL_ACTIVE_RESOURCES, &numAttirb);
+
+	std::cout << "\nInput Attributes: " << numAttirb << std::endl;
+
+	for (int i = 0; i < numAttirb; i++) {
+
+		GLint result[3];
+
+		glGetProgramResourceiv(prog,
+				GL_PROGRAM_INPUT, // program interface
+				i, // attribute index
+				3, // property count
+				properties,
+				3, // bufsize
+				nullptr, // ?
+				result);
+
+		GLint namelength = result[0];
+		char *attrib_name = new char[namelength];
+
+		glGetProgramResourceName(prog,
+				GL_PROGRAM_INPUT, // program interface
+				i, // attribute index
+				namelength, // bufsize
+				nullptr, // length in fact
+				attrib_name);
+
+		std::cout << "name (" << attrib_name << ")" << std::endl;
+		std::cout << "location(" << result[2] << ") type(" << ConvertTypeToString(result[1]) << ")" << std::endl;
+		delete [] attrib_name;
+	}
+}
+
+void GlRunner::LogProgramUniform(GLuint prog)
+{
+	GLint numUniform;
+	GLenum properties[] = {
+		GL_NAME_LENGTH,
+		GL_TYPE,
+		GL_LOCATION,
+		GL_BLOCK_INDEX
+	};
+
+	glGetProgramInterfaceiv(prog, GL_UNIFORM, GL_ACTIVE_RESOURCES, &numUniform);
+
+	std::cout << "\nUniform: " << numUniform << std::endl;
+
+	for (int i = 0; i < numUniform; i++) {
+
+		GLint result[4];
+
+		glGetProgramResourceiv(prog,
+				GL_UNIFORM, // program interface
+				i, // uniform index
+				4, // property count
+				properties,
+				4, // bufsize
+				nullptr, // ?
+				result);
+
+		GLint namelength = result[0];
+		char *uniform_name = new char[namelength];
+
+		glGetProgramResourceName(prog,
+				GL_UNIFORM, // program interface
+				i, // uniform index
+				namelength, // bufsize
+				nullptr, // length in fact
+				uniform_name);
+
+		std::cout << "name(" << uniform_name << ")" << std::endl;
+		std::cout << "block(" << result[3] << ") location(" << result[2] << ") type(" << ConvertTypeToString(result[1]) << ")" << std::endl;
+		delete [] uniform_name;
+	}
+}
+
+void GlRunner::LogProgramUniformBlock(GLuint prog)
+{
+	GLint numUniformBlock;
+
+	GLenum blockPropties[] = {
+		GL_NUM_ACTIVE_VARIABLES, // how many variables inside a block
+		GL_NAME_LENGTH // named block or anomynous block name length
+	};
+
+	glGetProgramInterfaceiv(prog, GL_UNIFORM_BLOCK, GL_ACTIVE_RESOURCES, &numUniformBlock);
+
+	std::cout << "\nUniform Block: " << numUniformBlock << std::endl;
+
+	for (int i = 0; i < numUniformBlock; i++) {
+
+		/* get block resource information first */
+		GLint blockinfo[2];
+
+		glGetProgramResourceiv(prog,
+				GL_UNIFORM_BLOCK, // program interface
+				i, // uniform block index
+				2, // property count
+				blockPropties,
+				2, // bufsize
+				nullptr, // ?
+				blockinfo);
+
+		char *uniformblock_name = new char[blockinfo[1]];
+
+		glGetProgramResourceName(prog,
+				GL_UNIFORM_BLOCK, // program interface
+				i, // uniform block index
+				blockinfo[1], // bufsize
+				nullptr, // length in fact
+				uniformblock_name);
+
+		std::cout << "name(" << uniformblock_name << ") variables(" << blockinfo[0] << ")" << std::endl;
+
+		delete [] uniformblock_name;
+	}
 }
 
 GLuint GlRunner::BuildProgramPipeline()
@@ -299,5 +438,85 @@ GLuint GlRunner::BuildXfb(const char *filename, GLsizei count, const char **vary
 	glUseProgram(ProgramObj);
 
 	return ProgramObj;
+}
+
+const char* GlRunner::ConvertTypeToString(GLenum type)
+{
+	switch (type) {
+		case GL_FLOAT: return "float";
+		case GL_FLOAT_VEC2: return "vec2";
+		case GL_FLOAT_VEC3: return "vec3";
+		case GL_FLOAT_VEC4: return "vec4";
+		case GL_DOUBLE: return "double";
+		case GL_DOUBLE_VEC2: return "dvec2";
+		case GL_DOUBLE_VEC3: return "dvec3";
+		case GL_DOUBLE_VEC4: return "dvec4";
+		case GL_INT: return "int";
+		case GL_INT_VEC2: return "ivec2";
+		case GL_INT_VEC3: return "ivec3";
+		case GL_INT_VEC4: return "ivec4";
+		case GL_UNSIGNED_INT: return "unsigned int";
+		case GL_UNSIGNED_INT_VEC2: return "uvec2";
+		case GL_UNSIGNED_INT_VEC3: return "uvec3";
+		case GL_UNSIGNED_INT_VEC4: return "uvec4";
+		case GL_BOOL: return "bool";
+		case GL_BOOL_VEC2: return "bvec2";
+		case GL_BOOL_VEC3: return "bvec3";
+		case GL_BOOL_VEC4: return "bvec4";
+		case GL_FLOAT_MAT2: return "mat2";
+		case GL_FLOAT_MAT3: return "mat3";
+		case GL_FLOAT_MAT4: return "mat4";
+		case GL_FLOAT_MAT2x3: return "mat2x3";
+		case GL_FLOAT_MAT2x4: return "mat2x4";
+		case GL_FLOAT_MAT3x2: return "mat3x2";
+		case GL_FLOAT_MAT3x4: return "mat3x4";
+		case GL_FLOAT_MAT4x2: return "mat4x2";
+		case GL_FLOAT_MAT4x3: return "mat4x3";
+		case GL_DOUBLE_MAT2: return "dmat2";
+		case GL_DOUBLE_MAT3: return "dmat3";
+		case GL_DOUBLE_MAT4: return "dmat4";
+		case GL_DOUBLE_MAT2x3: return "dmat2x3";
+		case GL_DOUBLE_MAT2x4: return "dmat2x4";
+		case GL_DOUBLE_MAT3x2: return "dmat3x2";
+		case GL_DOUBLE_MAT3x4: return "dmat3x4";
+		case GL_DOUBLE_MAT4x2: return "dmat4x2";
+		case GL_DOUBLE_MAT4x3: return "dmat4x3";
+		case GL_SAMPLER_1D: return "sampler1D";
+		case GL_SAMPLER_2D: return "sampler2D";
+		case GL_SAMPLER_3D: return "sampler3D";
+		case GL_SAMPLER_CUBE: return "samplerCube";
+		case GL_SAMPLER_1D_SHADOW: return "sampler1DShadow";
+		case GL_SAMPLER_2D_SHADOW: return "sampler2DShadow";
+		case GL_SAMPLER_1D_ARRAY: return "sampler1DArray";
+		case GL_SAMPLER_2D_ARRAY: return "sampler2DArray";
+		case GL_SAMPLER_1D_ARRAY_SHADOW: return "sampler1DArrayShadow";
+		case GL_SAMPLER_2D_ARRAY_SHADOW: return "sampler2DArrayShadow";
+		case GL_SAMPLER_2D_MULTISAMPLE: return "sampler2DMS";
+		case GL_SAMPLER_2D_MULTISAMPLE_ARRAY: return "sampler2DMSArray";
+		case GL_SAMPLER_CUBE_SHADOW: return "samplerCubeShadow";
+		case GL_SAMPLER_BUFFER: return "samplerBuffer";
+		case GL_SAMPLER_2D_RECT: return "sampler2DRect";
+		case GL_SAMPLER_2D_RECT_SHADOW: return "sampler2DRectShadow";
+		case GL_INT_SAMPLER_1D: return "isampler1D";
+		case GL_INT_SAMPLER_2D: return "isampler2D";
+		case GL_INT_SAMPLER_3D: return "isampler3D";
+		case GL_INT_SAMPLER_CUBE: return "isamplerCube";
+		case GL_INT_SAMPLER_1D_ARRAY: return "isampler1DArray";
+		case GL_INT_SAMPLER_2D_ARRAY: return "isampler2DArray";
+		case GL_INT_SAMPLER_2D_MULTISAMPLE: return "isampler2DMS";
+		case GL_INT_SAMPLER_2D_MULTISAMPLE_ARRAY: return "isampler2DMSArray";
+		case GL_INT_SAMPLER_BUFFER: return "isamplerBuffer";
+		case GL_INT_SAMPLER_2D_RECT: return "isampler2DRect";
+		case GL_UNSIGNED_INT_SAMPLER_1D: return "usampler1D";
+		case GL_UNSIGNED_INT_SAMPLER_2D: return "usampler2D";
+		case GL_UNSIGNED_INT_SAMPLER_3D: return "usampler3D";
+		case GL_UNSIGNED_INT_SAMPLER_CUBE: return "usamplerCube";
+		case GL_UNSIGNED_INT_SAMPLER_1D_ARRAY: return "usampler1DArray";
+		case GL_UNSIGNED_INT_SAMPLER_2D_ARRAY: return "usampler2DArray";
+		case GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE: return "usampler2DMS";
+		case GL_UNSIGNED_INT_SAMPLER_2D_MULTISAMPLE_ARRAY: return "usampler2DMSArray";
+		case GL_UNSIGNED_INT_SAMPLER_BUFFER: return "usamplerBuffer";
+		case GL_UNSIGNED_INT_SAMPLER_2D_RECT: return "usampler2DRect";
+	}
 }
 #endif
